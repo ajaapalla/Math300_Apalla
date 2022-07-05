f5cd4b39a9614116eb95dae2588cd0ef6dbf3285
ajaapalla <108547367+ajaapalla@users.noreply.github.com>
2022-07-05 11:46:06 -0700

    commit 1
    day 1 matlab

---
+++ matlab/math300_matlabintro.m
@@ -0,0 +1,19 @@
+a = [1 2 3];
+b = [4 5 6];
+c = [7 8 9];
+
+d = [a;b;c];
+disp(d);
+
+A = [1 2; 3 4];
+B = [5 6; 7 8];
+C = [1 2
+    3 4];
+D = [5 6
+    7 8];
+%both of these are the same 
+
+disp(A);
+disp(B);
+disp(C);
+disp(D);
